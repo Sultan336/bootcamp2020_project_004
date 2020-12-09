9a5d04dc635874830d9269f3509c8630c1d69ca4
Sultan <m.sultan338@gmail.com>
2020-12-10 00:02:31 +0500

    waapi in action
    created a moveable abject

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-//import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 
 function App() {
-  // const element = useRef(null);
-  // useEffect(() => {
-  //   element.current.focus();
-  // }, []);
-  const { ref, playState, getAnimation } = useWebAnimations({
+  const moonAnim = useWebAnimations({
     keyframes: [
-      { transform: "translate(0,0)" },
-      { transform: "translate(600px,0)" },
+      { transform: 'rotate(0deg)' },
+      { transform: 'rotate(360deg)' },
     ],
     timing: {
-      //delay: 500, // Start with a 500ms delay
-      duration: 1000, // Run for 1000ms
-      iterations: Infinity, // Repeat forever
-      direction: "alternate", // Run the animation forwards and then backwards
-      easing: "ease-in-out", // Use a fancy timing function
-    },
+      duration: 5000,
+      iterations: Infinity,
+      delay: 0,
+      easing: "linear",
+    }
   });
+
+
   return (
     <div >
-      {/* input box
-      <input ref={element} type='text'></input> */}
-      < div className='target' ref={ref}></div >
-      Current animation state: {playState} {``}
-      <button onClick={() => getAnimation().pause()}>Pause</button>
-      <button onClick={() => getAnimation().play()}>Play</button>
-    </div >
+      <div className="moon" ref={moonAnim.ref}></div>
+
+    </div>
   );
 }
 
